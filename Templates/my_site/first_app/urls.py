b52593b376e3e2d3a53c7a6804a4e93f10bbf125
Symbiont23707 <146iqmega1hp@gmail.com>
2023-01-12 13:39:01 +0300

    Filters

--- Templates/my_site/first_app/urls.py
+++ Templates/my_site/first_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.default_views)
-]
+    path('',views.default_views),
+    path('variables/',views.variable_views)
+    ]
--- Templates/my_site/first_app/views.py
+++ Templates/my_site/first_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def default_views(request):
     return render(request,'first_app/example.html')
+
+def variable_views(request):
+    my_var = {'first_name':'rosalind','second_name':'franklin','some_list':[1,2,3],'some_dict':{'inside_key':'inside_value'}}
+    return render(request,'first_app/variable.html',context=my_var)
