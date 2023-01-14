806e40f5eac0f374389a5bb4b434e627cfa7eeb7
Symbiont23707 <146iqmega1hp@gmail.com>
2023-01-14 18:36:59 +0300

    Tags and URL Names in Templates

--- Templates/my_site/first_app/urls.py
+++ Templates/my_site/first_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
+app_name = 'first_app'
+
 urlpatterns = [
-    path('',views.default_views),
-    path('variables/',views.variable_views)
+    path('',views.default_views,name='example'),
+    path('variable/',views.variable_views,name='variable')
     ]
--- Templates/my_site/first_app/views.py
+++ Templates/my_site/first_app/views.py
@@ -5,5 +5,4 @@ def default_views(request):
     return render(request,'first_app/example.html')
 
 def variable_views(request):
-    my_var = {'first_name':'rosalind','second_name':'franklin','some_list':[1,2,3],'some_dict':{'inside_key':'inside_value'},'user_logged_in':True}
-    return render(request,'first_app/variable.html',context=my_var)
+    return render(request,'first_app/variable.html')
