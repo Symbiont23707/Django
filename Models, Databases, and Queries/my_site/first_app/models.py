8eca1ee567a622f2daf1969353c24e2240ea714b
Symbiont23707 <146iqmega1hp@gmail.com>
2023-01-19 22:03:25 +0300

    Data Interaction Using .all()

--- Models, Databases, and Queries/my_site/first_app/models.py
+++ Models, Databases, and Queries/my_site/first_app/models.py
@@ -5,3 +5,6 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} is {self.age} years old."
