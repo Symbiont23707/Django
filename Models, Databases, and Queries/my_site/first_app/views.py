16bbb252179717c07704d7baefe4924fe8e8b7a7
Symbiont23707 <146iqmega1hp@gmail.com>
2023-01-22 14:19:59 +0300

    Connecting Templates and Database Models

--- Models, Databases, and Queries/my_site/first_app/views.py
+++ Models, Databases, and Queries/my_site/first_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+def list_patients(request):
+    list_patients = models.Patient.objects.all()
+    context = {'patients':list_patients}
+    return render(request,'first_app/patients.html',context=context)
