a088aa6b0fc85d5b58e343bb59dfe15a98bcedbe
Symbiont23707 <146iqmega1hp@gmail.com>
2023-01-24 03:55:51 +0300

    model-and-website-part-two(2/2)

--- Django Admin/my_car_site/cars/views.py
+++ Django Admin/my_car_site/cars/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from . import models
 
 # Create your views here.
 
 def add(request):
-    return render(request,"cars/add.html")
+    if request.POST:
+        brand = request.POST["brand"]
+        year = request.POST["year"]
+        models.Cars.objects.create(brand=brand,year=year)
+        return redirect(reverse("cars:list"))
+    else:
+        return render(request,"cars/add.html")
 
 def delete(request):
-    return render(request,"cars/delete.html")
+    if request.POST:
+        pk = request.POST["pk"]
+        try:
+            models.Cars.objects.get(pk=pk).delete()
+            return redirect(reverse("cars:list"))
+        except:
+            print(f"PK:{pk} not founded")
+            return redirect(reverse("cars:list"))
+    else:        
+        return render(request,"cars/delete.html")
 
 def list(request):
     all_cars = models.Cars.objects.all()
-    context_cars = {"all_car":all_cars}
+    context_cars = {"all_cars":all_cars}
     return render(request,"cars/list.html",context=context_cars)
