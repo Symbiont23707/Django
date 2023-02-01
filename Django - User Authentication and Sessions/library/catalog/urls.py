b0c1d0bdcbf8f9623e86f79b305e01fbe7e74fe3
Symbiont23707 <146iqmega1hp@gmail.com>
2023-02-01 03:50:58 +0300

    Page Setup

--- Django - User Authentication and Sessions/library/catalog/urls.py
+++ Django - User Authentication and Sessions/library/catalog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('create_book',views.BookCreateView.as_view(),name='create_view'),
+    path('book/<int:pk>/',views.BookDetailView.as_view(),name='book_detail')
 ]
--- Django - User Authentication and Sessions/library/catalog/views.py
+++ Django - User Authentication and Sessions/library/catalog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import BookInstance, Book
+from django.views.generic import CreateView, DetailView
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello!")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_avail = BookInstance.objects.filter(status__exact="a").count()
+
+    context = {
+        'num_books':num_books,
+        'num_instances':num_instances,
+        'num_instances_avail':num_instances_avail
+    }
+    return render(request,'catalog/index.html',context=context)
+
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = "__all__"
+
+class BookDetailView(DetailView):
+    model = Book
+    
